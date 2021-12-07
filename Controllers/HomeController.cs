d49f43f8b6f13152e379aec1d2af61a4edc747c9
Rajanikant <rajanikantpawar31@gmail.com>
2021-12-07 19:45:06 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment_3.Models;
+
+namespace Assignment_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult RegisterNow()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RegisterNow(GuestResponse guestResponse)
+        {
+            return View("Thanks", guestResponse);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment_3.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Enter Email Id")]
+        public string EmailId { get; set; }
+        [Required(ErrorMessage ="Enter Phone No.")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage ="Plese select option")]
+        public bool? willAttend { get; set; }
+    }
+}
